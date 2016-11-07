2ab6377924359e88f7b7ce79c2b58ffec8a64e4b
Ashley Hutton <ashley.hutton@ku.edu>
2016-11-06 19:44:28 -0600

    Fix question 1

--- adv-q1/main.cpp
+++ adv-q1/main.cpp
@@ -11,15 +11,15 @@ int main(){
 	// since we don't know the size, we will just set the array size to 100
 	// this would be something to get clarification on before implementing
 	std::string names[100];
-	int i=0;
-	while (std::cin.eof()){
-		std::cin >> names[i];
+	int size=0;
+	while (std::cin >> names[size]){
+		size++;
+	}
+	for (int i=0; i<=size; i++){
 		// if the index is even, that indicates a first name, so print
 		if (i%2 == 0){
 			std::cout << "Hello, " << names[i] << "!\n";
 		}
-		i++;
 	}
-
 
 }
